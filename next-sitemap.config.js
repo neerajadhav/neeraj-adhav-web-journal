1eb7e1e82a32a5a0748c737a5b2f2b412b52a460
neerajadhav <adhavneeraj9500@gmail.com>
2025-02-12 19:02:47 +0530

    new file:   next-sitemap.config.js 	modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   public/robots.txt 	new file:   public/sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://neerajadhav.in', // Replace with your domain
+  generateRobotsTxt: true, // Generates robots.txt file
+  sitemapSize: 5000, // Adjust based on the number of pages
+};
