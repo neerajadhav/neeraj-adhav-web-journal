bde98f7c40ffaea4af852ee3265c5ad0062ab8c1
neerajadhav <adhavneeraj9500@gmail.com>
2025-02-14 10:51:57 +0530

    modified:   src/app/mastodon/page.tsx

--- src/app/mastodon/page.tsx
+++ src/app/mastodon/page.tsx
@@ -1,6 +1,4 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import MastodonPost from './MastodonPost';
 import { Container } from '@/components/Container';
 import PageHeader from '@/components/PageHeader';
@@ -32,49 +30,45 @@ interface MastodonPostType {
   media_attachments: MediaAttachment[];
 }
 
-interface MastodonUserResponse {
-  id: string;
-}
-
-const MastodonFeed: React.FC = () => {
-  const [posts, setPosts] = useState<MastodonPostType[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await axios.get<MastodonUserResponse>(
-          'https://sciences.social/api/v1/accounts/lookup?acct=neerajadhav',
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
-
-        const userId = response.data.id;
-
-        const postsResponse = await axios.get<MastodonPostType[]>(
-          `https://sciences.social/api/v1/accounts/${userId}/statuses`,
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
-
-        setPosts(postsResponse.data);
-      } catch (error) {
-        console.error('Error fetching Mastodon posts:', error);
+const fetchPosts = async (): Promise<MastodonPostType[]> => {
+  try {
+    const userResponse = await fetch(
+      'https://sciences.social/api/v1/accounts/lookup?acct=neerajadhav',
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+        cache: 'no-store', // Ensures fresh data on each request
       }
-    };
+    );
 
-    fetchPosts();
-  }, []);
+    if (!userResponse.ok) throw new Error('Failed to fetch user data');
+
+    const userData = await userResponse.json();
+    const userId = userData.id;
+
+    const postsResponse = await fetch(
+      `https://sciences.social/api/v1/accounts/${userId}/statuses`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+        cache: 'no-store',
+      }
+    );
+
+    if (!postsResponse.ok) throw new Error('Failed to fetch posts');
+
+    return await postsResponse.json();
+  } catch (error) {
+    console.error('Error fetching Mastodon posts:', error);
+    return [];
+  }
+};
+
+const MastodonFeed = async () => {
+  const posts = await fetchPosts(); // Fetching posts on the server
 
   return (
     <Container>
-      <PageHeader />
-      <div className='mx-auto flex flex-col gap-6'>
+      {/* <PageHeader /> */}
+      <div className="mx-auto flex flex-col gap-6">
         {posts.map((post) => (
           <MastodonPost key={post.id} post={post} />
         ))}
