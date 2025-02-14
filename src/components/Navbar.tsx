34d15f4245cc93f6f5ccbe7c1133296d4bd6c9b5
neerajadhav <adhavneeraj9500@gmail.com>
2025-02-14 16:17:16 +0530

    modified:   src/components/Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import {
+  ArrowPathIcon,
   CodeBracketIcon,
   HomeIcon as HomeIconOutline,
   NewspaperIcon as NewspaperIconOutline,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState, useEffect } from 'react';
 
 import {
   HomeIcon as HomeIconSolid,
@@ -46,20 +48,41 @@ const NAVLINKS = [
 ];
 
 export const Navbar = () => {
+  const [loadingLink, setLoadingLink] = useState(null);
   const path = usePathname();
+
+  useEffect(() => {
+    setLoadingLink(null);
+  }, [path]);
+
+  const handleNavigation = (href: any) => {
+    setLoadingLink(href);
+  };
+
   return (
-    <div className='fixed bottom-0 lg:bottom-5 z-20 flex w-full justify-center'>
-      <nav className='flex w-full lg:w-auto flex-row justify-center gap-3 lg:rounded-full border border-zinc-100 bg-slate-100 px-3 py-2 md:gap-5 dark:border-slate-800 dark:bg-slate-900'>
+    <div className='fixed bottom-0 z-20 flex w-full justify-center lg:bottom-5'>
+      <nav className='flex w-full flex-row justify-center gap-3 border border-zinc-100 bg-slate-100 px-3 py-2 md:gap-5 lg:w-auto lg:rounded-full dark:border-slate-800 dark:bg-slate-900'>
         {NAVLINKS.map((navlink) => (
           <Link
             href={navlink.href}
             key={navlink.name}
+            passHref
+            onClick={() => handleNavigation(navlink.href)}
             className={`relative rounded-full p-3 shadow-sm dark:text-slate-400 dark:hover:bg-slate-50/10 ${path === navlink.href ? 'bg-slate-300 dark:bg-slate-50/10 dark:text-slate-50' : 'bg-white dark:bg-slate-950 dark:text-slate-400'}`}
             title={navlink.tooltip}
           >
-            {path === navlink.href ? navlink.activeIcon : navlink.icon}
-            <span className='sr-only'>{navlink.tooltip}</span>{' '}
-            {/* For accessibility */}
+            {loadingLink === navlink.href ? (
+              <div className='flex items-center justify-center'>
+                <div className='animate-spin rounded-full'>
+                  <ArrowPathIcon className='h-6 w-6' />
+                </div>
+              </div>
+            ) : (
+              <>
+                {path === navlink.href ? navlink.activeIcon : navlink.icon}
+                <span className='sr-only'>{navlink.tooltip}</span>
+              </>
+            )}
           </Link>
         ))}
       </nav>
