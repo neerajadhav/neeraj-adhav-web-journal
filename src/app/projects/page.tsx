ac1c28ca98e38a5261d30989b12165c38d6e574f
neerajadhav <adhavneeraj9500@gmail.com>
2025-02-15 15:44:22 +0530

    modified:   src/app/projects/page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,13 +5,13 @@ import { PROJECTS } from '@/utils/consts/index';
 export default function Project() {
   return (
     <Container>
-      <div className='flex flex-col gap-5 border bg-white px-4 py-4 dark:border dark:border-slate-800 dark:bg-slate-900'>
-        <div className='flex w-full flex-row items-center justify-between border-b pb-4 dark:border-slate-800'>
+      <div className='flex flex-col gap-5 border bg-white dark:border dark:border-slate-800 dark:bg-slate-900'>
+        <div className='flex w-full flex-row items-center justify-between border-b p-4 dark:border-slate-800'>
           <h2 className='text-2xl font-semibold dark:text-zinc-100'>
             Projects
           </h2>
         </div>
-        <div className='mx-auto flex w-full flex-col flex-wrap gap-5 sm:flex-row sm:justify-center md:gap-8'>
+        <div className='mx-auto flex w-full flex-col flex-wrap gap-5 sm:flex-row sm:justify-center md:gap-8 px-5 pb-5'>
           {PROJECTS.map((project) => (
             <SingleProject project={project} key={project.name} />
           ))}
