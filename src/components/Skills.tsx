e73d7ffa79892b63700ec355a95adb37daf557db
neerajadhav <adhavneeraj9500@gmail.com>
2025-02-15 19:39:24 +0530

    modified:   src/components/Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -76,7 +76,7 @@ export const Skills = () => {
         }}
       >
         {/* Skills Grid */}
-        <div className='grid w-full grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+        <div className='grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
           {skills.map((skillSet, index) => (
             <div
               key={index}
