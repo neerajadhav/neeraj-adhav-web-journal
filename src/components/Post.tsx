5bd7d975babba1b85a4cee0a2757b939740b7396
neerajadhav <adhavneeraj9500@gmail.com>
2025-02-19 18:18:10 +0530

    modified:   src/components/Post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -45,7 +45,7 @@ export const Post = (props: PostProps) => {
   return (
     <Link
       href={`/${postInfo.slug}`}
-      className={`flex ${props.first ? 'w-full lg:w-2/3' : 'w-full'} flex-col items-center gap-4 rounded-lg hover:opacity-90 p-4`}
+      className={`flex ${props.first ? 'w-full lg:w-2/3' : 'w-full'} flex-col items-center gap-4 rounded-lg hover:opacity-90 px-4 pt-4`}
     >
       <div className='flex aspect-video w-full overflow-hidden rounded-lg'>
         {postInfo.coverImage?.url ? (
