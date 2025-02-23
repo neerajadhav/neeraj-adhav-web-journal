74f9d7c60565e7cd8f20079a8f0c0bb448b65b0d
neerajadhav <adhavneeraj9500@gmail.com>
2025-02-23 15:33:37 +0530

    modified:   src/app/globals.css 	modified:   src/components/AboutMe.tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -33,7 +33,7 @@ export const AboutMe = () => {
               alt='profile pic'
               width={1000}
               height={1000}
-              className='flex-1 rounded-xl border dark:border-bgDark'
+              className='flex-1 rounded-full border dark:border-bgDark w-[150px]'
             />
             <div className='mb-4 flex flex-col items-center justify-between gap-2 sm:flex-row lg:gap-0'>
               {isAvailableLink && (
