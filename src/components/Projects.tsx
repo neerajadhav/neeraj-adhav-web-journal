e53aab72ebe3b8f546f8c0d273e0446ab2222a04
neerajadhav <adhavneeraj9500@gmail.com>
2025-02-26 12:56:24 +0530

    modified:   src/components/Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,7 +13,7 @@ export const Projects = () => {
         icon: <ArrowRightIcon className='h-4 w-4' />,
       }}
     >
-      <div className='mx-auto flex w-full flex-col gap-2 sm:flex-row lg:gap-0'>
+      <div className='mx-auto flex w-full flex-col gap-1 sm:flex-row lg:gap-0'>
         {PROJECTS.slice(0, 3).map((project) => (
           <SingleProject project={project} key={project.name} />
         ))}
