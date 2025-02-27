782973a376f595fcc1d9bf5297cc47baf9bb1474
neerajadhav <adhavneeraj9500@gmail.com>
2025-02-27 20:05:43 +0530

    modified:   src/csomponents/DarkModeBtn.tsx

--- src/components/DarkModeBtn.tsx
+++ src/components/DarkModeBtn.tsx
@@ -29,7 +29,7 @@ export function ModeToggle() {
               <CgDarkMode className='h-5 w-5' />
             )}
           </span>
-          <span>{theme === 'light' ? 'Night' : 'Day'}</span>
+          <span>{theme === 'light' ? 'Dark' : 'Light'} Mode</span>
         </div>
       </button>
     </>
