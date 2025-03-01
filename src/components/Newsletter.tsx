1abc302e01de41e7a0adfee29ea90d6005980f35
neerajadhav <adhavneeraj9500@gmail.com>
2025-03-01 23:07:08 +0530

    modified:   src/components/Newsletter.tsx

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,20 +1,24 @@
-
 const Newsletter = () => {
   return (
-    <section className='mb-3 w-full select-none rounded-lg border bg-[#f6f6f6] p-3 dark:border-bgDark dark:bg-bgDark lg:mb-2'>
-      <div className='mx-auto flex flex-col items-center justify-center gap-3 lg:flex-row'>
-        <h2 className='text text-center font-semibold'>Newsletter</h2>
-        <form className='flex w-full items-center gap-3'>
+    <section className='mb-4 w-full select-none rounded-lg border bg-gray-100 p-4 dark:border-gray-700 dark:bg-bgDark lg:mb-3'>
+      <div className='mx-auto flex flex-col items-center justify-center gap-4 lg:flex-row'>
+        {/* Title */}
+        <h2 className='text-center text-lg font-semibold text-gray-900 dark:text-gray-200'>
+          Newsletter
+        </h2>
+
+        {/* Form Section */}
+        <form className='flex w-full items-center gap-4'>
           <input
             type='email'
             placeholder='Service currently disabled!'
             disabled
-            className='w-19/20 h-10 flex-1 rounded-lg border border-gray-700 bg-white px-2 focus:outline-none focus:ring-1 focus:ring-blue-500 dark:bg-bgDark'
+            className='h-10 w-full flex-1 rounded-lg border border-gray-400 bg-white px-3 text-gray-600 focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400'
           />
           <button
             type='submit'
             disabled
-            className='text-md w-1/20 flex items-center justify-center gap-2 rounded-lg border border-blue-400 bg-blue-500 px-3 py-2 font-medium text-white transition-all hover:border-blue-500 hover:bg-blue-500 hover:text-white hover:shadow-lg hover:shadow-blue-400 dark:border-gray-600 dark:bg-white dark:text-black dark:hover:bg-white dark:hover:text-black md:flex lg:w-1/6'
+            className='flex h-10 w-auto items-center rounded-lg border border-blue-400 bg-blue-500 px-4 py-2 font-medium text-white transition-all hover:shadow-lg dark:border-gray-600 dark:bg-gray-200 dark:text-black dark:hover:bg-white dark:hover:text-black'
           >
             Subscribe
           </button>
