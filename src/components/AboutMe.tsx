0f9b9bbc02c2bc42d009c1ef966cd84c93da4743
neerajadhav <adhavneeraj9500@gmail.com>
2025-03-01 22:50:48 +0530

    modified:   src/components/AboutMe.tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -31,7 +31,7 @@ export const AboutMe = () => {
   const availableFor = publication.author?.availableFor || null;
 
   return (
-    <div className='flex flex-col items-center gap-4 rounded-xl border bg-white p-4 dark:border-0 dark:bg-bgDark'>
+    <div className='flex flex-col items-center gap-4 rounded-xl border bg-white p-4 dark:border-0 dark:bg-bgDark shadow'>
       <div className='flex w-full flex-col items-start gap-4 lg:flex-row'>
         {publication.author.profilePicture && (
           <div className='b mx-auto flex w-1/2 flex-col items-center gap-3 overflow-hidden rounded-xl p-3 sm:max-w-52'>
@@ -94,20 +94,20 @@ export const AboutMe = () => {
                 {publication.author.location}
               </p>
             )}
-            <div className='mt-3 flex w-full select-none flex-row gap-3 lg:gap-6'>
+            <div className='mt-4 flex gap-4'>
               {resumeLink && (
                 <Link
                   href={resumeLink}
-                  className='text-md flex w-1/2 items-center justify-center gap-2 rounded-full border border-gray-400 px-3 py-2 font-medium text-gray-700 transition-all hover:border-blue-500 hover:bg-blue-500 hover:text-white hover:shadow-lg hover:shadow-blue-400 dark:border-gray-600 dark:text-white dark:hover:bg-white dark:hover:text-black md:flex md:px-4 md:py-2 lg:w-1/6'
+                  className='rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-md transition-all hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600'
                 >
-                  <FaFilePdf className='h-4 w-4' /> Resume
+                  <FaFilePdf className='mr-2 inline-block' /> Resume
                 </Link>
               )}
               <Link
                 href='mailto:neerajadhav@duck.com'
-                className='text-md flex w-1/2 items-center justify-center gap-2 rounded-full border border-blue-400 bg-blue-500 px-3 py-2 font-medium text-white transition-all hover:border-blue-500 hover:bg-blue-500 hover:text-white hover:shadow-lg hover:shadow-blue-400 dark:border-gray-600 dark:bg-white dark:text-black dark:hover:bg-white dark:hover:text-black md:flex md:px-4 md:py-2 lg:w-1/6'
+                className='rounded-md bg-gray-700 px-4 py-2 text-sm font-medium text-white shadow-md transition-all hover:bg-gray-800 dark:bg-gray-600 dark:hover:bg-gray-700'
               >
-                <FaEnvelope className='h-4 w-4' /> Email
+                <FaEnvelope className='mr-2 inline-block' /> Email Me
               </Link>
             </div>
           </div>
