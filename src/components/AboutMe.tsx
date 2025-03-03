ec076916308d410006ebc5b9a17b4352f1a13b65
neerajadhav <adhavneeraj9500@gmail.com>
2025-03-03 22:58:41 +0530

    modified:   src/components/AboutMe.tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,8 +1,6 @@
 'use client';
 import {
-  faBook,
-  faLocationDot,
-  faUniversity,
+  faLocationDot
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Inknut_Antiqua } from 'next/font/google';
