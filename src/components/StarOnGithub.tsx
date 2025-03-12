ba2be4fc58515241771e12e339a85a74c6d4c753
neerajadhav <adhavneeraj9500@gmail.com>
2025-03-12 15:42:48 +0530

    modified:   src/components/StarOnGithub.tsx

--- src/components/StarOnGithub.tsx
+++ src/components/StarOnGithub.tsx
@@ -13,7 +13,7 @@ const StarOnGithub = () => {
   return (
     <button
       onClick={handleStarClick}
-      className={`relative flex items-center gap-1 rounded-lg border p-1 px-2 font-sans text-sm shadow-sm dark:border-0 dark:text-gray-400 dark:hover:bg-gray-50/10 lg:shadow-none ${'bg-white hover:bg-white dark:bg-transparent dark:text-gray-400'}`}
+      className={`relative flex items-center gap-1 rounded-lg border p-1 px-2 font-sans text-sm shadow-sm dark:border-0 dark:text-gray-400 dark:hover:bg-gray-50/10 lg:shadow-none print:hidden ${'bg-white hover:bg-white dark:bg-transparent dark:text-gray-400'}`}
     >
       <AiFillStar className='text-yellow-400' size={20} />
       Star on GitHub
