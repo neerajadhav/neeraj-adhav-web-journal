3985de2469e1fd27928a5aeb8775ada815367acf
neerajadhav <adhavneeraj9500@gmail.com>
2025-03-13 10:22:46 +0530

    modified:   src/components/SocialLinks.tsx

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,31 +1,29 @@
 'use client';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { PublicationLinks } from '../../generated/graphq';
 import { SOCIAL_LINKS } from '../utils/consts';
-import { PublicationLinks, usePublicationQuery } from '../../generated/graphq';
-import { FaMastodon } from 'react-icons/fa';
-const host = process.env.NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST as string;
 
 export const SocialLinks = () => {
-  const { data } = usePublicationQuery({
-    host,
-  });
+  const socialLinks: Partial<Record<keyof PublicationLinks, string>> = {
+    hashnode: 'https://hashnode.com/@neerajadhav',
+    github: 'https://github.com/neerajadhav',
+    linkedin: 'https://www.linkedin.com/in/neerajadhav/',
+    mastodon: 'https://sciences.social/@neerajadhav',
+  };
 
-  const mastodonlink = 'https://sciences.social/@neerajadhav';
-
-  if (!data?.publication?.links) return null;
-  const links = Object.entries(data.publication.links).filter(
-    ([key, value]) => value
+  const links = Object.entries(socialLinks).filter(([, value]) =>
+    Boolean(value)
   ) as Array<[keyof PublicationLinks, string]>;
 
   return (
     <div className='flex gap-2'>
       {links.map(([socialName, socialLink]) => (
         <a
-          href={socialLink as string}
+          key={socialName}
+          href={socialLink}
           target='_blank'
           className='flex rounded-full p-2 text-gray-950 hover:bg-gray-600 hover:text-white hover:shadow-sm dark:text-zinc-300 dark:hover:bg-gray-950 dark:hover:text-zinc-300'
-          key={socialName}
         >
           <FontAwesomeIcon
             icon={SOCIAL_LINKS[socialName]!}
@@ -33,15 +31,6 @@ export const SocialLinks = () => {
           />
         </a>
       ))}
-      <a
-        href={mastodonlink}
-        rel='me'
-        target='_blank'
-        className='flex rounded-full p-2 text-gray-950 hover:bg-gray-600 hover:text-white hover:shadow-sm dark:text-zinc-300 dark:hover:bg-gray-950 dark:hover:text-zinc-300'
-        key={mastodonlink}
-      >
-        <FaMastodon className='h-5 w-5' />
-      </a>
     </div>
   );
 };
